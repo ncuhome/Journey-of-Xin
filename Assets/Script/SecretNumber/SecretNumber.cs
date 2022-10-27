a6ec8bf5fdf7382c5c113aa5fe2b43f7a665f6a6
FeatherRian <1178441170@qq.com>
2022-10-27 08:08:42 +0800

    🐛 修复部分对话bug🐛 修复神秘数字bug

--- Assets/Script/SecretNumber/SecretNumber.cs
+++ Assets/Script/SecretNumber/SecretNumber.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SecretNumber : MonoBehaviour
 {
     private DialogueTrigger correctDialog = null;
     private DialogueTrigger wrongDialog = null;
-    
+
     public void Click()
     {
         if (InputManager.Instance.sceneState != SceneState.MainScene) { return; }
         if (SceneItemManager.Instance.itemStates[GetComponent<ItemDisplay>().itemIndex] != ItemState.Interactive || !SceneItemManager.Instance.interactive) { return; }
+        Debug.Log("SecretClick");
         if (StoreSystem.Instance.Find(20))
-        {   
+        {
             correctDialog.StartDialogue();
         }
         else
@@ -26,5 +28,22 @@ public class SecretNumber : MonoBehaviour
     {
         correctDialog = transform.Find("CorrectDialog").GetComponent<DialogueTrigger>();
         wrongDialog = transform.Find("WrongDialog").GetComponent<DialogueTrigger>();
+    }
+
+    void Start()
+    {
+        GetComponent<Image>().alphaHitTestMinimumThreshold = 0;
+    }
+
+    void Update()
+    {
+        if (StoreSystem.Instance.Find(20))
+        {
+            GetComponent<Image>().color = new Color(1, 1, 1, 1);
+        }
+        else
+        {
+            GetComponent<Image>().color = new Color(1, 1, 1, 0);
+        }
     }
 }
